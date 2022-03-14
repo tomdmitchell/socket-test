0fb17c43a16fbca79ade9d817d063b16c5b53c0a
tomdmitchell <thomasdavymitchell@gmail.com>
2022-03-14 09:05:19 +0000

    add working version - no disconnect id handler

--- index.js
+++ index.js
@@ -4,27 +4,60 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+const { nanoid } = require('nanoid');
+
+//user count
+let userCount = 0;
+let userInfo = [];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 app.use(express.static(__dirname + '/public'));
 
-
+//connection
 io.on('connection', (socket) => {
-  //init user
-  console.log('a user connected');
+  //user connect listener
+  userCount++;
+  io.emit('user data', { userCount: userCount }); // This will emit the event to all connected sockets
+  socket.on('userLogin', (data) => {
+    const idsUsed = userInfo.map((info) => info.id);
+    if (!idsUsed.includes(data)) {
+      console.log(data);
+      const coloursUsed = userInfo.map((info) => info.colour);
+      if (coloursUsed.includes('green')) {
+        userInfo.push({ id: data, colour: 'red' });
+        socket.emit('user id', { id: data, colour: 'red' });
+      } else {
+        userInfo.push({ id: data, colour: 'green' });
+        socket.emit('user id', { id: data, colour: 'green' });
+      }
+    }
+    console.log('user info: ', userInfo);
+    if (userInfo.length > 2) {
+      throw `Error: only two users can be connected`;
+    }
+  });
+
   //disconnect listener
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    userCount--;
+    io.emit('user data', { userCount: userCount }); // This will emit the event to all connected sockets
   });
-  //event emitter
-  socket.on('click listener', (msg) => {
-    console.log(msg)
-    io.emit('click listener', 'they are clicking')
-  })
+
+  //click listener
+  socket.on('click handler', (msg) => {
+    console.log(msg);
+    io.emit('click handler', msg);
+  });
+  //game win
+  socket.on('game over', (msg) => {
+    console.log(msg);
+    io.emit('game over', msg);
+  });
 });
 
+//server listen
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
